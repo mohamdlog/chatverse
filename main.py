b1a7cf74aa712963b635bbf4772bb02c3b872240
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-27 14:17:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ if system == 'windows':
     boost = input((r'Enter path to boost (i.e., C:\boost_1_81_0)') + '\n')
     project = input((r'Enter path to chatverse (i.e., C:\Users\name\Desktop\chatverse)') + '\n')
     print(f"""  
-            1. Open developer command line
+            1. Open developer command prompt
             2. Type "cl /EHsc /I {boost} {project}\server.cpp"
             3. Type "server"
-            4. Open a second developer command line
+            4. Open a second developer command prompt
             5. Type "cl /EHsc /I {boost} {project}\client.cpp"
             6. Type "client"
 """)
-#elif system == 'linux':
+elif system == 'linux':
+
