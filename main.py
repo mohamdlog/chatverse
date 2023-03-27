d70c746638cc47d1c62d7c9424358146b9118a34
mohamdlog <127714779+mohamdlog@users.noreply.github.com>
2023-03-27 14:09:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+
+
+system = input("Are you using Windows or Linux?\n").lower()
+if system == 'windows':
+    boost = input((r'Enter path to boost (i.e., C:\boost_1_81_0)') + '\n')
+    project = input((r'Enter path to chatverse (i.e., C:\Users\name\Desktop\chatverse)') + '\n')
+    print(f"""  
+            1. Open developer command line
+            2. Type "cl /EHsc /I {boost} {project}\server.cpp"
+            3. Type "server"
+            4. Open a second developer command line
+            5. Type "cl /EHsc /I {boost} {project}\client.cpp"
+            6. Type "client"
+""")
+#elif system == 'linux':
